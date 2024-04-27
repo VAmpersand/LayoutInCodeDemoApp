dee55a6e28cb8f5277e67111b92c4b2ab4c1effe
Viktor <viktor.prikolota@exinity.com>
2024-04-26 21:11:29 -0300

    Initial Commit

---
+++ LayoutInCodeDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutInCodeDemoApp
+//
+//  Created by Viktor Prikolota on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
